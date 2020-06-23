09f9fe2a1d122ee41620b4891d022d44445237a5
Vaishu <vaishupriya90@gmail.com>
2020-06-23 13:18:17 -0500

    add a new project IputOutput

---
+++ InputOutput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare all variables at the top
+            // Put our prompt in to a variable and use the variable in the WriteLine
+            // Let's declare our name variable at the top, but assign it with the ReadLine
+            // Let's create a variable to hold our greeting;
+            string prompt = "What is your name?";
+            string name;
+            string greeting = "Hiya";
+
+            Console.WriteLine(prompt);
+
+            name = Console.ReadLine();
+
+            Console.WriteLine(greeting + " " + name);
+        }
+    }
+}
