f7fe31359ba421c1ddf408562513c82a17daa417
Vaishu <vaishupriya90@gmail.com>
2020-06-23 13:40:26 -0500

    added a new project NumericTypes

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double areaOfRectangle;
+            string input;
+            double length;
+            double breadth;
+            Console.WriteLine("Enter the length for the rectangle:");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+            Console.WriteLine("Enter the Breadth for the rectangle:");
+            input = Console.ReadLine();
+            breadth = double.Parse(input);
+            areaOfRectangle = length * breadth;
+            Console.WriteLine("Area of the rectangle is: " + areaOfRectangle);
+            //Console.ReadLine();
+
+
+
+
+
+            
+        }
+    }
+}
